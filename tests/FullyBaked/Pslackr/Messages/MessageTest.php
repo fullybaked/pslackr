25c394d0270966ff02b2eb39139d07f691d852e5
Dave Baker <david@fullybaked.co.uk>
2014-03-01 21:17:56 +0000

    Create test for Message class

---
+++ tests/FullyBaked/Pslackr/Messages/MessageTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FullyBaked\Pslackr\Messages;
+
+use PHPUnit_Framework_TestCase;
+use Exception;
+
+class MessageTest extends PHPUnit_Framework_TestCase
+{
+    public function testAsJson()
+    {
+        $message = new Message();
+        $message->text('Message text goes here');
+        $result = $message->asJson();
+        $expected = '{"text":"Message text goes here"}';
+        $this->assertEquals($result, $expected);
+
+        $message->text('Change the message and check');
+        $result = $message->asJson();
+        $expected = '{"text":"Change the message and check"}';
+        $this->assertEquals($result, $expected);
+    }
+}

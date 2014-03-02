23c7b8b95b78ca4c6a99c4bea4e5b36403a79b72
Dave Baker <david@fullybaked.co.uk>
2014-03-02 15:46:17 +0000

    Replace Message class as an interface
    Change Message class to be an interface outlining what Slack messages
    should look like. The old "Message" class will change to CustomMessage
    and implement this interface

--- src/FullyBaked/Pslackr/Messages/Message.php
+++ src/FullyBaked/Pslackr/Messages/Message.php
@@ -2,20 +2,39 @@
 
 namespace FullyBaked\Pslackr\Messages;
 
-class Message
+interface Message
 {
-    protected $text;
+    /**
+     * return the data as a JSON string to send as message
+     * to Slack's API
+     */
+    public function asJson();
 
-    public function text($text)
-    {
-        $this->text = $text;
-    }
+    /**
+     * set the required 'text' parameter of the message
+     */
+    public function text($messageText);
 
-    public function asJson()
-    {
-        $message = [
-            'text' => $this->text
-        ];
-        return json_encode($message);
-    }
+    /**
+     * set an optional 'channel' parameter of the message
+     */
+    public function channel($channelName);
+
+    /**
+     * set the optional name of the bot that will appear in
+     * the channel logs with the message
+     */
+    public function username($userName);
+
+    /**
+     * provide a URL to an icon/avatar to use for the message
+     * user
+     */
+    public function iconUrl($url);
+
+    /**
+     * provide an :emoji: token that Slack will understand
+     * to use as the icon/avatar for the message user
+     */
+    public function iconEmoji($emojiToken);
 }

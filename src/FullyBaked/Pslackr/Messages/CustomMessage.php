c93f71e233a8ff2aee9c5a40ad1b7be034fb2c6b
Dave Baker <david@fullybaked.co.uk>
2014-03-02 16:47:11 +0000

    Implement setter methods for optional params

--- src/FullyBaked/Pslackr/Messages/CustomMessage.php
+++ src/FullyBaked/Pslackr/Messages/CustomMessage.php
@@ -6,18 +6,38 @@ class CustomMessage implements Message
 {
     protected $text;
 
+    protected $optionalFields = [];
+
+    public function __construct($messageText)
+    {
+        $this->text = $messageText;
+    }
     public function asJson()
     {
-        $message = [
-            'text' => $this->text
-        ];
+        $message = ['text' => 'This is a test message'];
+
+        $message = array_merge($message, $this->optionalFields);
+
         return json_encode($message);
     }
 
-    public function text($text)
+    public function channel($channelName)
     {
-        $this->text = $text;
+        $this->optionalFields['channel'] = $channelName;
     }
 
+    public function username($username)
+    {
+        $this->optionalFields['username'] = $username;
+    }
 
+    public function iconEmoji($token)
+    {
+        $this->optionalFields['icon_emoji'] = $token;
+    }
+
+    public function iconUrl($url)
+    {
+        $this->optionalFields['icon_url'] = $url;
+    }
 }

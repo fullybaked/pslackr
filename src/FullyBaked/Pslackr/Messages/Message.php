af4dbef74c1ed5a4f065c1af38e1cce27399a549
Dave Baker <david@fullybaked.co.uk>
2014-03-06 22:18:41 +0000

    Remove the optional methods from interface

--- src/FullyBaked/Pslackr/Messages/Message.php
+++ src/FullyBaked/Pslackr/Messages/Message.php
@@ -7,29 +7,8 @@ interface Message
     /**
      * return the data as a JSON string to send as message
      * to Slack's API
+     *      - return must be a json string with at least a text: key value pair
+     * @return string
      */
     public function asJson();
-
-    /**
-     * set an optional 'channel' parameter of the message
-     */
-    public function channel($channelName);
-
-    /**
-     * set the optional name of the bot that will appear in
-     * the channel logs with the message
-     */
-    public function username($userName);
-
-    /**
-     * provide a URL to an icon/avatar to use for the message
-     * user
-     */
-    public function iconUrl($url);
-
-    /**
-     * provide an :emoji: token that Slack will understand
-     * to use as the icon/avatar for the message user
-     */
-    public function iconEmoji($emojiToken);
 }

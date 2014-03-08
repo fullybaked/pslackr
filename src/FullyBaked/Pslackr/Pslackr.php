3f05f7e3f8b50c80d45f76d3f08242d791b6e8de
Dave Baker <david@fullybaked.co.uk>
2014-03-08 22:07:52 +0000

    Add dependency on an http client to Pslackr class

--- src/FullyBaked/Pslackr/Pslackr.php
+++ src/FullyBaked/Pslackr/Pslackr.php
@@ -8,10 +8,13 @@ class Pslackr implements Transport
 
     protected $domain;
 
-    public function __construct($config)
+    protected $httpClient;
+
+    public function __construct($config, $httpClient)
     {
         $this->token = $config['token'];
         $this->domain = $config['domain'];
+        $this->client = $httpClient;
     }
 
     public function send(FullyBaked\Pslackr\Messages\Message $message)

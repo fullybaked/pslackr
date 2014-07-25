101ce992436e8d5f5b94db0c5dbc28ddd05e0bfd
Dave Baker <david@fullybaked.co.uk>
2014-07-25 22:53:28 +0100

    Removed deprecated test as it was breaking build

--- tests/FullyBaked/Pslackr/PslackrTest.php
+++ tests/FullyBaked/Pslackr/PslackrTest.php
@@ -8,14 +8,6 @@ class PslackrTest extends PHPUnit_Framework_TestCase
 {
     public function testConstructorSetsConfigProperty()
     {
-        $config = ['domain' => 'tester', 'token' => '123456'];
 
-        $relection = new \ReflectionClass('FullyBaked\Pslackr\Pslackr');
-        $tokenAttr = $relection->getProperty('token');
-        $tokenAttr->setAccessible(true);
-
-        $slack = new Pslackr($config);
-
-        $this->assertEquals($config['token'], $tokenAttr->getValue($slack));
     }
 }
